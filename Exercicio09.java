424f386b857bb16c8da8a6140746f23dbaca685d
Patricia Sousa <111404527+pspsousa@users.noreply.github.com>
2024-03-12 10:35:50 -0300

    Update Exercicio09.java

--- Exercicio09.java
+++ Exercicio09.java
@@ -3,19 +3,19 @@ import java.util.Scanner;
 
 public class Exercicio09 {
 	public static void main(String[] args) {
-		//Escreva um algoritmo para ler as dimensões de um quadrado (lado), calcular e escrever a área do quadrado. Dica a=l²
+		//Algoritmo para ler as dimensões de um quadrado (lado), calcular e escrever a área do quadrado. Dica a=l²
 		try {
-			Scanner sc = new Scanner(System.in);
+			Scanner scanner = new Scanner(System.in);
 			System.out.println("Descubra a area do quadrado!");
 			System.out.print("Qual tamanho do lado? ");
 			
-			int ladoQuadrado = sc.nextInt();
-			int calc = ladoQuadrado * ladoQuadrado;
+			int Lado = scanner.nextInt();
+			int Calculo = Lado * Lado;
 			
-			System.out.println("A area do quadrado (" + ladoQuadrado + " * " + ladoQuadrado + ") = " + calc);
-			sc.close();
+			System.out.println("A area do quadrado (" + Lado + " * " + Lado + ") = " + Calculo);
+			scanner.close();
 		} catch (Exception e) {
-			System.out.println("Digite um valor valido!");
+			System.out.println("Valor invalido!");
 		}
 	}
 }
