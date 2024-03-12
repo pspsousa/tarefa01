ec4d700feaabcd1c86ecdc50d796128c04b3c3b7
Patricia Sousa <111404527+pspsousa@users.noreply.github.com>
2024-03-12 10:19:24 -0300

    Update Exercicio01.java

--- Exercicio01.java
+++ Exercicio01.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 
 public class Exercicio01 {
 	public static void main(String[] args) {
-		//Escreva um algoritmo para ler um valor do teclado e exibir como resposta o número sucessor.
+		//Algoritmo para ler um valor do teclado e exibir como resposta o número sucessor.
 		try {
-			Scanner sc = new Scanner(System.in);
+			Scanner scanner = new Scanner(System.in);
 			
 			System.out.println("Digite um valor para descobrir o sucessor: ");
-			int value = sc.nextInt();
-			int successorValue = value + 1;
-			System.out.println("O sucessor do valor " + value + " = " + successorValue);
+			int Valor = scanner.nextInt();
+			int Sucessor = Valor + 1;
+			System.out.println("O sucessor é " + Valor + " = " + Sucessor);
 			
-			sc.close();
+			scanner.close();
 		} catch (Exception e) {
 			System.out.println("Digite um valor valido!");
 		}
