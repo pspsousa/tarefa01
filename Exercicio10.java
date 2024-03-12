ddefd6cdb76b4ee92e968afb7d7cea7a117f030f
Patricia Sousa <111404527+pspsousa@users.noreply.github.com>
2024-03-12 10:38:10 -0300

    Update Exercicio10.java

--- Exercicio10.java
+++ Exercicio10.java
@@ -3,23 +3,23 @@ import java.util.Scanner;
 
 public class Exercicio10 {
 	public static void main(String[] args) {
-		//Escreva um algoritmo para ler as dimensões de um losango (diagonal maior, diagonal menor), calcular e escrever a área do losango. Dica a=D.d/2
+		//Algoritmo para ler as dimensões de um losango (diagonal maior, diagonal menor), calcular e escrever a área do losango. Dica a=D.d/2
 		try {
-			Scanner sc = new Scanner(System.in);
+			Scanner scanner = new Scanner(System.in);
 			
-			System.out.println("Basta digitar as diagonais do losango abaixo e depois clicar em \"Calcular Área\".");
+			System.out.println("Vamos calcular as dimensoes de um losango");
 			System.out.print("Diagonal 1: ");
-			float diagonal1 = sc.nextFloat();
+			float Diagonal1 = scanner.nextFloat();
 			
 			System.out.print("Diagonal 2: ");
-			float diagonal2 = sc.nextFloat();
+			float Diagonal2 = scanner.nextFloat();
 			
-			float calc = (diagonal1 * diagonal2) / 2;
+			float Calculo = (Diagonal1 * Diagonal2) / 2;
 			
-			System.out.println("A area do losango ("+ diagonal1 + " * " + diagonal2 + ")/2 = " + calc);
-			sc.close();
+			System.out.println("A area do losango ("+ Diagonal1 + " * " + Diagonal2 + ")/2 = " + Calculo);
+			scanner.close();
 		} catch (Exception e) {
-			System.out.println("Digite um valor valido!");
+			System.out.println("Valor invalido!");
 		}
 	}
 }
