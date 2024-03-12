a701f2cfcbd7903fed6691a35beaaf238f79cb6c
Patricia Sousa <111404527+pspsousa@users.noreply.github.com>
2024-03-12 10:31:42 -0300

    Update Exercicio07.java

--- Exercicio07.java
+++ Exercicio07.java
@@ -3,23 +3,23 @@ import java.util.Scanner;
 
 public class Exercicio07 {
 	public static void main(String[] args) {
-		//Escreva um algoritmo para ler as dimensões de um retângulo (base e altura), calcular e escrever a área do retângulo. Dica a=b.h
+		//Algoritmo para ler as dimensões de um retângulo (base e altura), calcular e escrever a área do retângulo. Dica a=b.h
 		try {
-			Scanner sc = new Scanner(System.in);
+			Scanner scanner = new Scanner(System.in);
 			System.out.println("Vamos calcular a base do retangulo!");
 			
 			System.out.print("Digite a base maior: ");
-			float baseMaior = sc.nextFloat();
+			float BaseMaior = scanner.nextFloat();
 			System.out.print("Digite a base menor: ");
-			float baseMenor = sc.nextFloat();
+			float BaseMenor = scanner.nextFloat();
 
-			float calc = baseMaior * baseMenor;
+			float Calculo = BaseMaior * BaseMenor;
 			
-			System.out.println("A base do retangulo tem: " + calc + "m²");
-			sc.close();
+			System.out.println("A base do retangulo tem: " + Calculo + "m²");
+			scanner.close();
 			
 		} catch (Exception e) {
-			System.out.println("Digite um valor valido!");
+			System.out.println("Valor invalido!");
 		}
 	}
 }
