e30bde1c723fdced0234effdad5ed93ae0da8726
Patricia Sousa <111404527+pspsousa@users.noreply.github.com>
2024-03-12 10:21:33 -0300

    Update Exercicio02.java

--- Exercicio02.java
+++ Exercicio02.java
@@ -2,17 +2,17 @@ package tarefa01;
 import java.util.Scanner;
 
 public class Exercicio02 {
-	//Escreva um algoritmo para ler um valor do teclado e exibir como resposta o número antecessor.
+	//Algoritmo para ler um valor do teclado e exibir como resposta o número antecessor.
 	public static void main(String[] args) {
 		try {
-			Scanner sc = new Scanner(System.in);
+			Scanner scanner = new Scanner(System.in);
 			
 			System.out.println("Digite um valor para saber o antecessor: ");
-			int valor = sc.nextInt();
-			int antecessor = valor - 1;
-			System.out.println("O valor antecessor do " + valor + " = " + antecessor);
+			int Valor = scanner.nextInt();
+			int Antecessor = Valor - 1;
+			System.out.println("O valor é " + Valor + " = " + Antecessor);
 			
-			sc.close();
+			scanner.close();
 		} catch (Exception e) {
 			System.out.println("Valor invalido!");
 		}	
