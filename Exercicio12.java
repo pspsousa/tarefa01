6c28210d24d01dc39f2bc43d91f50d8a60a50876
Patricia Sousa <111404527+pspsousa@users.noreply.github.com>
2024-03-12 10:42:36 -0300

    Update Exercicio12.java

--- Exercicio12.java
+++ Exercicio12.java
@@ -3,21 +3,21 @@ import java.util.Scanner;
 
 public class Exercicio12 {
 	public static void main(String[] args) {
-		//Escreva um algoritmo para ler as dimensões de um circulo (raio), calcular e escrever a área do circulo. a=pi*r²
+		//Algoritmo para ler as dimensões de um circulo (raio), calcular e escrever a área do circulo. a=pi*r²
 		try {
-			Scanner sc = new Scanner(System.in);
+			Scanner scanner = new Scanner(System.in);
 			
-			System.out.println("Descubra a area do circulo!");
+			System.out.println("Vamos calcular a area do circulo!");
 			System.out.print("Digite o raio: ");
-			float raio = sc.nextFloat();
-			double pi = Math.PI;
-			double calc = pi * Math.pow(raio, 2);
+			float Raio = scanner.nextFloat();
+			double Pi = Math.PI;
+			double Calculo = Pi * Math.pow(Raio, 2);
 			
-			System.out.println("A area do circulo: " + calc);
-			sc.close();
+			System.out.println("A area do circulo: " + Calculo);
+			scanner.close();
 			
 		} catch (Exception e) {
-			System.out.println("Digite um valor valido!");
+			System.out.println("Valor invalido!");
 		}
 	}
 }
